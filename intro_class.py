69138ebb78f8c6dff74c7956f66cb1769446ed2d
Jonathan-creator111 <hapsjune@gmail.com>
2025-05-15 17:07:40 +0300

    first commit

---
+++ intro_class.py
@@ -0,0 +1,19 @@
+print("Jonathan Mbugua")
+print(100)
+print(1000)
+
+x=20
+y=5
+z=x+y
+print(z)
+my_student="Jonathan"
+print(my_student)
+#calculateareaofasquare
+side=20
+area=side*side
+print(area)
+#calculateareaofrectangle
+length=30
+width=10
+area=length*width
+print(area)
