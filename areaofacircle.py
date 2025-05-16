02a21f579324ccbb778ae0c016f7265ccdc97a84
Jonathan-creator111 <hapsjune@gmail.com>
2025-05-16 17:05:41 +0300

    area of circle

--- areaofacircle.py
+++ areaofacircle.py
@@ -1,4 +1,5 @@
 #calculate area of a circle
 radius=10
-area=3.14*radius*radius
+pi=3.14
+area=pi*radius**2
 print(area)
---
+++ stings.py
@@ -0,0 +1,28 @@
+first_name="Jonathan"
+second_name="Mbugua"
+p=99.78
+x=1000
+z=False
+print(type(second_name))
+print(type(p))
+print(type(x))
+print(type(z))
+full_name=first_name+" "+second_name
+print(full_name)
+
+x="1000"
+y="2000"
+z=x+y
+print(z)
+
+text="python programming"
+print(text[10])
+print(text[7:18])
+print(text[0:6])
+
+#string methods
+my_text = "HELlo"
+my_text=my_text.capitalize()
+my_text=my_text.casefold()
+my_text=my_text.count("l")
+print(my_text)
