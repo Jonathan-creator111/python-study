1fee8e3287031c3ce10e2dcd58d855771f4ec6c1
Jonathan-creator111 <hapsjune@gmail.com>
2025-05-30 12:36:05 +0300

    loops.py

--- loops.py
+++ loops.py
@@ -10,3 +10,41 @@ print(numbers)
 nums=list(range(20,41))
 for i in range(20,41):
     print(i)
+
+
+
+
+
+#1Write a program that displays a numbers 1 to 50 inside a list.
+i=list(range(1,51))
+print(i)
+#2From 1 above display the ones divisible by 7 or 5 inside a list.
+odd=list(range(1,51))
+for i in odd:
+    if i%2!=0:
+        print(i)
+#3Find sum and average of values in the range between 10 to 40.
+values=list(range(10,41))
+for i in values:
+    print(i)
+#4Put in a list the first 10 odd numbers between 10 to 50. 
+odd_numbers=[]
+numb=list(range(10,51))
+for i in numb:
+    if i%2!=0:
+        odd_numbers.append(i)
+        if len(odd_numbers)==10:
+            break
+
+print(odd_numbers)
+#5write a program that takes a number as input and prints its multiplication table up to 10 using a for loop.
+
+#6write a program that counts and prints the number of even numbers between 1 and 50 using a for loop
+even_numbers=list(range(1,51))
+for i in even_numbers:
+    if i%2==0:
+        print(i)
+#7ls1 = [ (“Jay”, ‘20’), (“Mo”, ‘30’), (“Mya”, ‘32’) ]
+#Display the total quantity of the 3 above.
+ls1 = [ ("Jay", '20'), ("Mo", '30'), ("Mya", '32') ]
+print(type(ls1))
