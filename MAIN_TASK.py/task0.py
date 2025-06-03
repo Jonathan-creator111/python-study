ac9335d7f030538a6d4cbab248e1f24aa9b88b72
Jonathan-creator111 <hapsjune@gmail.com>
2025-06-03 03:17:38 +0300

    task0.py

---
+++ MAIN_TASK.py/task0.py
@@ -0,0 +1,9 @@
+#TASK 1: Using Python or PHP or Java or Ruby or JavaScript
+#Write a program that prompts the user to enter the base and height of a triangle and returns its area.
+#Once you learn functions,revisit this and write this code inside a function.
+base=input("enter the base: ")
+base=int(base)
+height=input("enter the height: ")
+height=int(height)
+area=1/2*base*height
+print(area)
